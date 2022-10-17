e4c0152dc780a68a1883b0baf940474bd781da47
Crowdbotics <engineering@crowdbotics.com>
2022-10-17 17:40:36 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gentest5_dev_39086/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gentest5_dev_39086;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gentest5_dev_39086";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gentest5_dev_39086",
+  url: "https://gentest5_dev_39086.botics.co",
+  api: "https://gentest5_dev_39086.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
